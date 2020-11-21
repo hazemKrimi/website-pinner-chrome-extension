fc64c248a6599e884518156536e611630d64bcb3
Hazem Krimi <krimihazem1@gmail.com>
2020-11-21 23:38:06 +0100

    Add incognito detection

--- background.js
+++ background.js
@@ -1,15 +1,15 @@
 chrome.contextMenus.create({
     title: 'Pin',
-    onclick: page => {
+    onclick: (page, tab) => {
         chrome.windows.create({
             url: page.pageUrl,
             width: 800,
             height: 600,
+            incognito: tab.incognito,
             focused: true,
             type: 'popup'
         }, window => {
-            chrome.tabs.onUpdated.addListener(async(id, changeInfo, tab) => {
-                console.log(id, changeInfo, tab);
+            chrome.tabs.onUpdated.addListener(async (id, changeInfo, tab) => {
                 if (id === window.tabs[0].id && changeInfo.status === 'complete')
                     await fetch('http://localhost:8000/', {
                         method: 'POST',
